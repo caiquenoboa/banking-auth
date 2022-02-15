4da37639e721fcf285f5aa248cdc2e33adddd8b1
Caique Noboa <caique.noboa@mercadolivre.com>
2022-02-15 15:20:19 -0300

    all project

---
+++ dto/login.go
@@ -0,0 +1,39 @@
+package dto
+
+import (
+	"errors"
+
+	"github.com/caiquenoboa/banking-auth/domain"
+	"github.com/dgrijalva/jwt-go"
+)
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type LoginResponse struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token,omitempty"`
+}
+
+type RefreshTokenRequest struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+}
+
+func (r RefreshTokenRequest) IsAccessTokenValid() *jwt.ValidationError {
+
+	// 1. invalid token.
+	// 2. valid token but expired
+	_, err := jwt.Parse(r.AccessToken, func(token *jwt.Token) (interface{}, error) {
+		return []byte(domain.HMAC_SAMPLE_SECRET), nil
+	})
+	if err != nil {
+		var vErr *jwt.ValidationError
+		if errors.As(err, &vErr) {
+			return vErr
+		}
+	}
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/caiquenoboa/banking-auth
+
+go 1.17
+
+require (
+	github.com/caiquenoboa/go-banking v0.0.0-20220215142241-ee30fa4f3cd9
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+)
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/jmoiron/sqlx v1.3.4
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.21.0 // indirect
+)
